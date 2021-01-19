66b238d4966b72048ded94f3709faa8af6cb6841
Lychar85 <63816497+Lychar85@users.noreply.github.com>
2021-01-19 17:35:22 +0100

    .

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const
     port = 4000
-    express = require('express'),
+express = require('express'),
     mongoose = require("mongoose"),
+    bodyParser = require('body-parser'),
     app = express();
 
+//app use-------------------------------------------------------------------------------------------------------------------------------------------------
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
+//VIEWS---------------------------------------------------------------------------------------------------------------------------------------------------
+//date--------------------------------------------
+var Handlebars = require("handlebars");
+var MomentHandler = require("handlebars.moment");
+MomentHandler.registerHelpers(Handlebars);
 
-
-    app.use(express.static(__dirname + "/public"));
-//VIEWS-------------------------------------------------------------
-const
-    exphbs = require("express-handlebars"),
-    Handlebars = require("handlebars"),
+// Handlebars--------------------------------------------
+exphbs = require("express-handlebars"),
     {
         allowInsecurePrototypeAccess
     } = require('@handlebars/allow-prototype-access');
 
-// Handlebars-------------------------------------------------------------
 app.engine('hbs', exphbs({
     defaultLayout: 'main',
     extname: 'hbs',
@@ -24,28 +31,50 @@ app.engine('hbs', exphbs({
 }));
 app.set('view engine', 'hbs')
 
-
+//connect mongoose---------------------------------------
 require('./config/db')
 
+//models-------------------------------------------------
+const post = require('./models/article')
 
+//route---------------------------------------------------------------------------------------------------------------------------------------------------
+//index--------------------------------------------------
+app.get('/', async (req, res) => {
+    const posts = await post.find({})
+    res.render('index', {
+        posts
+    })
+});
 
-
+//contact------------------------------------------------
 app.get('/contact', (req, res) => {
     res.render('contact')
+});
+
+//Ajout article------------------------------------------
+app.get('/articles/add', (req, res) => {
+        res.render('articles/add')
+    })
+    .post('/article/post', (req, res) => {
+        post.create(req.body, (err, docs) => {
+            res.redirect('/')
+        })
+
+        console.log(req.body)
+    });
+
+//ArticleOne---------------------------------------------
+app.get('/articles/:id', async (req, res) => {
+    const article = await post.findById(req.params.id)
+    console.log(req.params);
+    res.render('articles', {
+        article
+    })
 })
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
 
 
-
-
-
-
-
-
-
+//ouvre le port---------------------------------------------------------------------------------------------------------------------------------------------------
 app.listen(port, () => {
     console.log(`serveur connecter au port ${port}`);
 })
--- models/article.js
+++ models/article.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const articleModel = new mongoose.Schema({
    title: String,
-   intro: String,
    content: String,
+   author: String,
+   createDate: {
+      type: Date,
+      default: new Date()
+   }
 })
 
 const article = mongoose.model('article', articleModel)
