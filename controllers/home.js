c8f65fe16569e54aa4a89722a8bc3daf63496358
Lychar85 <63816497+Lychar85@users.noreply.github.com>
2021-01-27 17:47:03 +0100

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,5 @@ const post = require('../models/article');
 module.exports = async (req, res) => {
     const posts = await post.find({}).lean()
 
-    res.render('index', {
-        posts
-    })
+    res.render('index', {posts})
 }
