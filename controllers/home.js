6159b81a5f761c2ec306e09d6eb991ca2167e046
Lychar85 <63816497+Lychar85@users.noreply.github.com>
2021-01-28 10:16:33 +0100

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,7 @@
 const post = require('../models/article');
 
 module.exports = async (req, res) => {
-    const posts = await post.find({}).lean()
+    const posts = await post.find({}).lean() //.limit(5) filtrer pour  afficher seulement 5 produit
 
     res.render('index', {posts})
 }
